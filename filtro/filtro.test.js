6cd7d047cba5cec301ce3858795c8d34822532be
Spinkers <lucasmouraolopes@gmail.com>
2019-11-05 20:23:19 -0300

    teste

--- filtro/filtro.test.js
+++ filtro/filtro.test.js
@@ -1,12 +1,12 @@
 const filtro = require('./filtro');
 
-const contatos = [{"nome": "paulo", "sobrenome": "camargo"}, {"nome": "Lucas", "sobrenome": "Lopes"}];
+const contatos = [{"nome": "Giovanni", "sobrenome": "Manganotti"}, {"nome": "Lucas", "sobrenome": "Lopes"}];
 
-const encontrado = {"nome": "paulo", "sobrenome": "camargo"};
+const encontrado = {"nome": "Lucas", "sobrenome": "Lopes"};
 
 describe('Filtro', () => {
     test('Pesquisar um nome que existe na base', () => {
-        expect(filtro(contatos, "paulo")).toEqual(encontrado)
+        expect(filtro(contatos, "Lucas")).toEqual(encontrado)
     });
 
     test('Pesquisar um nome que não existe na base', () => {
